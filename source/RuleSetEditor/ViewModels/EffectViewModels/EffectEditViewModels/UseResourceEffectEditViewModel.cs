c1da63a9472caf8a2db00e463f5862c093b5a894
AliveDevil <git@alivedevil.de>
2015-11-30 13:11:16 +0100

    Add more editviewmodels.

---
+++ source/RuleSetEditor/ViewModels/EffectViewModels/EffectEditViewModels/UseResourceEffectEditViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RuleSetEditor.ViewModels.EffectViewModels.EffectEditViewModels
+{
+    public class UseResourceEffectEditViewModel : RuleSetViewModelBase
+    {
+        private UseResourceEffectViewModel useResourceEffect;
+
+        public UseResourceEffectViewModel UseResourceEffect
+        {
+            get { return useResourceEffect; }
+            set { if (!RaiseSetIfChanged(ref useResourceEffect, value)) return; }
+        }
+    }
+}
---
+++ source/RuleSetEditor/ViewModels/EffectViewModels/EffectEditViewModels/WorkplaceEffectEditViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RuleSetEditor.ViewModels.EffectViewModels.EffectEditViewModels
+{
+    public class WorkplaceEffectEditViewModel : RuleSetViewModelBase
+    {
+        private WorkplaceEffectViewModel workplaceEffect;
+
+        public WorkplaceEffectViewModel WorkplaceEffect
+        {
+            get { return workplaceEffect; }
+            set { if (!RaiseSetIfChanged(ref workplaceEffect, value)) return; }
+        }
+    }
+}
