c17a7606b7dadebe8f80d3fba94e92dada9c4ff9
AliveDevil <git@alivedevil.de>
2016-02-09 14:13:21 +0100

    DateTimeConditionView

--- source/RuleSetEditor/ViewModels/ConditionViewModels/DateTimeConditionViewModel.cs
+++ source/RuleSetEditor/ViewModels/ConditionViewModels/DateTimeConditionViewModel.cs
@@ -1,8 +1,29 @@
-﻿using RuleSet.Conditions;
+﻿using System;
+using Reactive.Bindings;
+using RuleSet.Conditions;
 
 namespace RuleSetEditor.ViewModels.ConditionViewModels
 {
     public class DateTimeConditionViewModel : ConditionViewModel<DateTimeCondition>
     {
+        private ReactiveProperty<DateTime> dateTimeProperty;
+
+        public ReactiveProperty<DateTime> DateTimeProperty
+        {
+            get
+            {
+                return dateTimeProperty;
+            }
+            private set
+            {
+                RaiseSetIfChanged(ref dateTimeProperty, value);
+            }
+        }
+
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+            DateTimeProperty = ReactiveProperty.FromObject(Condition, c => c.DateTime);
+        }
     }
 }
